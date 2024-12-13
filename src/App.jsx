2a7f97d3f2aec038f53712531eb4f830689d4598
vikram <vikramgreenliker44@gmail.com>
2024-12-13 16:12:23 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <h1 className="text-4xl">
+        React starter template with Tailwind CSS, Eslint and Prettier setup.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
